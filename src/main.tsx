25ef3746a7eef6fc423a12a50006b2087eebcfb7
Markciccio <markciccio@users.noreply.github.com>
2026-09-08 14:29:38 +0200

    Simplify event start flow

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './style.css'
 import './features.css'
+import { enableAnonymousRider } from './optionalRider'
 createRoot(document.getElementById('app')!).render(<StrictMode><App /></StrictMode>)
+enableAnonymousRider()
